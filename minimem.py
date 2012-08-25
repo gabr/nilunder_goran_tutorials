3b6092af939b43063535e7ed2fa6e67821a2412d
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-08-25 21:12:27 +0200

    zakonczenie gry i liczenie ruchow

--- minimem.py
+++ minimem.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+import random
+
 board_visible = list("________")
 board = ['A', 'B', 'C', 'D'] * 2
+steps = 0
+
+random.shuffle(board)   # miksuje elementy listy
 
 flipped = []
 
 while True:
     print " "+" ".join(board_visible)
     print " 0 1 2 3 4 5 6 7"
+
+    if not '_' in board_visible:
+        print " > You win in: %d steps" % steps
+        break
 
     if len(flipped) == 2:
         a, b = [board[i] for i in flipped]
@@ -27,6 +36,7 @@ while True:
         if idx < len(board) and board_visible[idx] == "_":
             board_visible[idx] = board[idx]
             flipped.append(idx)
+            steps += 1
             continue
     
-    print(" ! wrong input")
+    print " ! wrong input"
