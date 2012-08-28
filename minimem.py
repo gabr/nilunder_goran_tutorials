aecad4420eaf05bb707e21c800101ad9eec713ad
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-08-28 20:38:20 +0200

    zmiana czesci kodu na funkcje

--- minimem.py
+++ minimem.py
@@ -5,17 +5,32 @@ import random
 
 board_visible = list("________")
 board = ['A', 'B', 'C', 'D'] * 2
+
 steps = 0
 
 random.shuffle(board)   # miksuje elementy listy
 
 flipped = []
 
-while True:
-    print " "+" ".join(board_visible)
+def printBoard(board):
+    
+    print " "+" ".join(board)
     print " 0 1 2 3 4 5 6 7"
 
-    if not '_' in board_visible:
+def boardIsComplete(board):
+
+    return (not '_' in board)
+
+def inputIsValid():
+
+    return user_input.isdigit() and \
+            int(user_input) < len(board) and \
+            board_visible[int(user_input)] == "_"
+while True:
+
+    printBoard(board_visible)
+
+    if boardIsComplete(board_visible):
         print " > You win in: %d steps" % steps
         break
 
@@ -31,12 +46,11 @@ while True:
     if user_input == 'q':
         break
 
-    if user_input.isdigit():
+    if inputIsValid():
+
         idx = int(user_input)
-        if idx < len(board) and board_visible[idx] == "_":
-            board_visible[idx] = board[idx]
-            flipped.append(idx)
-            steps += 1
-            continue
-    
-    print " ! wrong input"
+        board_visible[idx] = board[idx]
+        flipped.append(idx)
+        steps += 1
+    else:
+        print " ! wrong input"
