92d48e807fd9ffbe4b3dfe9acba7fcc67646c71e
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-09-14 20:48:09 +0200

    Nowy wyglad planszy

--- minimem.py
+++ minimem.py
@@ -3,6 +3,46 @@
 
 import random
 
+images = [
+            [
+                "============",
+                "=    oo    =",
+                "=   o  o   =",
+                "=  oooooo  =",
+                "=  o    o  =",
+                "=  o    o  =",
+                "============"
+            ],
+            [
+                "============",
+                "=  ooooo   =",
+                "=  o    o  =",
+                "=  ooooo   =",
+                "=  o    o  =",
+                "=  ooooo   =",
+                "============"
+            ],
+            [
+                "============",
+                "=   ooooo  =",
+                "=  o       =",
+                "=  o       =",
+                "=  o       =",
+                "=   ooooo  =",
+                "============"
+            ],
+            [
+                "============",
+                "=  ooooo   =",
+                "=  o    o  =",
+                "=  o    o  =",
+                "=  o    o  =",
+                "=  ooooo   =",
+                "============"
+            ]
+        ]
+
+
 class Player:
 
     def __init__(self):
@@ -28,10 +68,21 @@ class Board(list):
 
     def __init__(self, cells):
         super(Board, self).__init__(cells)
+        random.shuffle(self)   # miksuje elementy listy
+        for i, cell in enumerate(self): cell.setIndex(i)
 
     def __str__(self):
-        return " ".join([str(c) for c in self]) +\
-                "\n0 1 2 3 4 5 6 7"
+        return "".join(["~" for line in range(38)])\
+                +"\n"+"\n".join([line for line in self._getLines(0,3)])\
+                +"\n"+"".join(["~" for line in range(38)])\
+                +"\n"+"\n".join([line for line in self._getLines(3,6)])\
+                +"\n"+"".join(["~" for line in range(38)])\
+                +"\n"+"\n".join([line for line in self._getLines(6,8)])
+
+    def _getLines(self, start, stop):
+        iters = [iter(cell) for cell in self[start:stop]]
+        while True:
+            yield " ".join([next(itr) for itr in iters])
 
     def isCompleted(self):
         for cell in self:
@@ -41,36 +92,56 @@ class Board(list):
 
 class Cell:
 
-    def __init__(self, char):
-        self.char  = char
-        self.image = '_'
+    image_covered = [
+            "============",
+            "============",
+            "============",
+            "============",
+            "============",
+            "============",
+            "============"
+        ]
+
+    def __init__(self, image):
+        self.image = image
+        self.image_visible = self.image_covered
+
+    def __iter__(self):
+        return iter(self.image_visible)
 
     def __str__(self):
-        return self.image
+        return "\n".join(self.image_visible)
 
     def __eq__(self, other):
-        return self.image == other.image
+        return self.image== other.image
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    def setIndex(self, idx):
+        self.image_covered = list(self.image_covered)
+        self.flip(); self.flip()
+#self.image_covered[3] = self.image_covered[3].format(idx)
+        self.image_covered[3] = "=====("+str(idx)+")===="
+
     def flip(self):
-        if self.image == '_':
-            self.image = self.char
+        if self.image_visible == self.image_covered:
+            self.image_visible = self.image
         else:
-            self.image = '_'
+            self.image_visible = self.image_covered
 
     def isCovered(self):
-        return self.image == '_'
+        return self.image_visible == self.image_covered
 
 ###############################################################################
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+cell_A = Cell(images[0])
+cell_B = Cell(images[1])
 
 player = Player()
 
-board = Board([Cell(c) for c in ['A', 'B', 'C', 'D'] * 2])
+board = Board([Cell(img) for img in images*2])
 
-random.shuffle(board)   # miksuje elementy listy
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
